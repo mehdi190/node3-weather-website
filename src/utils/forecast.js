4c707bc46db4277297f10f0a64109e921eaa4216
mehdi190 <mehdi73jabarooti@gmail.com>
2021-09-18 12:16:39 +0430

    Forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. it is currently ${body.current.temperature} degrees out. it feels like ${body.current.feelslike} degrees out and humidity is ${body.current.humidity}.`
+        `${body.current.weather_descriptions[0]}. it is currently ${body.current.temperature} degrees out. it feels like ${body.current.feelslike} degrees out.The humidity is ${body.current.humidity} %.`
       );
     }
   });
